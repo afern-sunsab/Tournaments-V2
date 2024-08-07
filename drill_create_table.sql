a199c1a91d6b95c3bda53e94b0598271eeda5de8
SunsabCharles <charles@sunsab.com>
2024-08-07 13:54:53 -0600

    Updates drill data script

--- drill_create_table.sql
+++ drill_create_table.sql
@@ -10,6 +10,21 @@ DROP TABLE IF EXISTS DB_Pad CASCADE;
 DROP TABLE IF EXISTS DB_Group CASCADE;
 
 -- Create tables
+CREATE TABLE DB_Group (
+    Group_ID INT PRIMARY KEY,
+	Group_Name VARCHAR(255)
+);
+
+CREATE TABLE DB_Pad (
+    Pad_ID INT PRIMARY KEY,
+    Group_ID INT REFERENCES DB_Group(Group_ID)
+);
+
+CREATE TABLE DB_Well (
+    Well_ID INT PRIMARY KEY,
+	Pad_ID INT REFERENCES DB_Pad(Pad_ID)
+);
+
 CREATE TABLE DB_Survey_Design (
     Survey_ID INT PRIMARY KEY,
     Lateral_Length double precision,
@@ -19,7 +34,8 @@ CREATE TABLE DB_Survey_Design (
     Lateral_Azimuth double precision,
     Total_Depth double precision,
     Drill_Build double precision,
-    Lateral_Dip double precision
+    Lateral_Dip double precision,
+	Well_ID INT REFERENCES DB_Well(Well_ID)
 );
 
 CREATE TABLE DB_Fracture_Design (
@@ -28,23 +44,8 @@ CREATE TABLE DB_Fracture_Design (
     Backoff double precision,
     Inclination_Cutoff double precision,
     Joint_Length double precision,
-    Survey_Design JSON
-);
-
-CREATE TABLE DB_Well (
-    Well_ID INT PRIMARY KEY,
-    Survey_ID INT REFERENCES DB_Survey_Design(Survey_ID),
-    Fracture_ID INT REFERENCES DB_Fracture_Design(Fracture_ID)
-);
-
-CREATE TABLE DB_Pad (
-    Pad_ID INT PRIMARY KEY,
-    Well_ID INT REFERENCES DB_Well(Well_ID)
-);
-
-CREATE TABLE DB_Group (
-    Group_ID INT PRIMARY KEY,
-    Pad_ID INT REFERENCES DB_Pad(Pad_ID)
+    Survey_Design JSON,
+	Well_ID INT REFERENCES DB_Well(Well_ID)
 );
 
 CREATE TABLE DB_Survey_Measurements (
@@ -60,7 +61,8 @@ CREATE TABLE DB_Survey_Measurements (
     NS double precision,
     EW double precision,
     Delta_X double precision,
-    Delta_Y double precision
+    Delta_Y double precision,
+	Well_ID INT REFERENCES DB_Well(Well_ID)
 );
 
 CREATE TABLE DB_Fracture_Measurements (
@@ -71,7 +73,8 @@ CREATE TABLE DB_Fracture_Measurements (
     NS_Surface_Plus double precision,
     NS_Surface_Minus double precision,
     TVD_Subsea_Plus double precision,
-    TVD_Subsea_Minus double precision
+    TVD_Subsea_Minus double precision,
+	Well_ID INT REFERENCES DB_Well(Well_ID)
 );
 
 CREATE TABLE DB_Survey_Parameters (
@@ -79,7 +82,8 @@ CREATE TABLE DB_Survey_Parameters (
     Survey_Design_ID INT REFERENCES DB_Survey_Design(Survey_ID),
     Surface_X double precision,
     Surface_Y double precision,
-    KB_Elevation double precision
+    KB_Elevation double precision,
+	Well_ID INT REFERENCES DB_Well(Well_ID)
 );
 
 CREATE TABLE DB_Fracture_Parameters (
@@ -87,5 +91,6 @@ CREATE TABLE DB_Fracture_Parameters (
     Fracture_Design_ID INT REFERENCES DB_Fracture_Design(Fracture_ID),
     Half_Length double precision,
     Azimuth double precision,
-    Height double precision
+    Height double precision,
+	Well_ID INT REFERENCES DB_Well(Well_ID)
 );
